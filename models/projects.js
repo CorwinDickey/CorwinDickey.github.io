127e15ac2056d9ac822229d4098b42e4d23e7e7d
CorwinDickey <62188881+CorwinDickey@users.noreply.github.com>
2021-02-18 06:02:24 -0600

    Added simon_sequencer to projects model. Converted index.html to index.ejs, need to test if it still works on github.io

---
+++ models/projects.js
@@ -0,0 +1,10 @@
+const projects = {
+    simon_sequencer: {
+        name: 'Simon Sequencer'
+        ,img: '.assets/project_images/simon_sequencer.jpg'
+        ,deployedDate: 'Feb 12, 2021'
+        ,createdDate: 'Feb 5, 2021'
+        ,lastEditedDate: 'Feb 17, 2021'
+        ,description: 'Web emulation of the Simon game. This was my first real project that I developed from scratch. It was also the first project created for my General Assembly Software Engineering Immersive course.'
+    }
+}
